1180b9ad83a487a840650c61161bf0cc057a1c13
watcharaAy <79580552+Watchara-Ay@users.noreply.github.com>
2022-05-04 18:26:16 +0700

    Update forgetpassword.dart

--- lib/screen/forgetpassword.dart
+++ lib/screen/forgetpassword.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:nine2five/model/profile.dart';
+import 'package:nine2five/screen/login.dart';
 
 class forgetpassword extends StatelessWidget {
   final formkey = GlobalKey<FormState>();
@@ -15,39 +16,85 @@ class forgetpassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Forget Password Page"),
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 255, 110, 110),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(10, 100, 10, 10),
-          child: SingleChildScrollView(
-            child: Row(
-              children: [
-                const Text("Username", style: TextStyle(fontSize: 20)),
-                TextFormField(
-                  validator:
-                      RequiredValidator(errorText: "Please fill something!!!"),
-                  onSaved: (username) {
-                    profile.username = username!;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text("Password", style: TextStyle(fontSize: 20)),
-                TextFormField(
-                  validator:
-                      RequiredValidator(errorText: "Please fill something!!!"),
-                  obscureText: true,
-                  onSaved: (password) {
-                    profile.password = password!;
-                  },
-                ),
-              ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Forget Password Page"),
+        backgroundColor: Color.fromARGB(255, 255, 110, 110),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Form(
+            key: formkey,
+            child: SingleChildScrollView(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Username", style: TextStyle(fontSize: 20)),
+                    TextFormField(
+                      validator: RequiredValidator(
+                          errorText: "Please fill something!!!"),
+                      onSaved: (username) {
+                        profile.username = username!;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text("Email", style: TextStyle(fontSize: 20)),
+                    TextFormField(
+                      validator: MultiValidator([
+                        RequiredValidator(errorText: "Please fill email"),
+                        EmailValidator(errorText: "Wrong email formation")
+                      ]),
+                      keyboardType: TextInputType.emailAddress,
+                      onSaved: (email) {
+                        profile.email = email!;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text("Password", style: TextStyle(fontSize: 20)),
+                    TextFormField(
+                      validator: RequiredValidator(
+                          errorText: "Please fill something!!!"),
+                      obscureText: true,
+                      onSaved: (password) {
+                        profile.password = password!;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color.fromARGB(255, 255, 110, 110)),
+                        ),
+                        child: const Text("Submit"),
+                        onPressed: () {
+                          if (formkey.currentState!.validate()) {
+                            formkey.currentState!.save();
+                            print("username = ${profile.username} "
+                                "email = ${profile.email} "
+                                "password = ${profile.password}");
+                            formkey.currentState!.reset();
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return LoginPage();
+                            }));
+                          }
+                        },
+                      ),
+                    ),
+                  ]),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
