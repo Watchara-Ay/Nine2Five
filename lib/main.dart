dcb42897bbf7f4b43b1e486652cea4458420770f
watcharaAy <79580552+Watchara-Ay@users.noreply.github.com>
2022-05-15 23:06:47 +0700

    3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nine2five/screen/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
