977a0eeaba64e92cb3552d4948b3e26533e801bd
watcharaAy <79580552+Watchara-Ay@users.noreply.github.com>
2022-05-17 23:46:08 +0700

    emu

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,12 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:nine2five/screen/forgetpassword.dart';
 import 'package:nine2five/screen/today.dart';
 import 'package:nine2five/screen/register.dart';
 import 'package:nine2five/model/information.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final formkey = GlobalKey<FormState>();
+
   information info = information(
       username: '',
       firstname: '',
@@ -15,6 +23,41 @@ class LoginPage extends StatelessWidget {
       gender: '',
       email: '',
       password: '');
+
+  Future<void> initFirebase() async {
+    await Firebase.initializeApp();
+    print('Firebase initialized');
+  }
+
+  Future<void> login() async {
+    try {
+      print('Call login');
+      print(info.email);
+      print(info.password);
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: info.email, password: info.password);
+      print(credential);
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return const homePage();
+      }));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('#######################');
+    print('Init state!!');
+    print('#######################');
+    initFirebase();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,13 +87,13 @@ class LoginPage extends StatelessWidget {
               const SizedBox(
                 height: 10,
               ),
-              const Text("Username",
+              const Text("Email",
                   style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
               TextFormField(
                 validator:
                     RequiredValidator(errorText: "Please fill something!!!"),
-                onSaved: (username) {
-                  info.username = username!;
+                onChanged: (email) {
+                  info.email = email;
                 },
               ),
               const SizedBox(
@@ -62,8 +105,8 @@ class LoginPage extends StatelessWidget {
                 validator:
                     RequiredValidator(errorText: "Please fill something!!!"),
                 obscureText: true,
-                onSaved: (password) {
-                  info.password = password!;
+                onChanged: (password) {
+                  info.password = password;
                 },
               ),
               const SizedBox(
@@ -106,10 +149,7 @@ class LoginPage extends StatelessWidget {
                   icon: const Icon(Icons.login),
                   label: const Text("Login", style: TextStyle(fontSize: 32)),
                   onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const homePage();
-                    }));
+                    login();
                   },
                 ),
               ),
